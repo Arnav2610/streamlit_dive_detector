50657f9788f6063778e947aac07bc8c7b7cbd5e9
Arnav2610 <arnavk2610@gmail.com>
2025-02-23 05:20:49 -0500

    added requirements.txt

--- predict.py
+++ predict.py
@@ -8,7 +8,7 @@ import joblib
 import json
 import google.generativeai as genai
 
-genai.configure(api_key="GEMINI_API_KEY")  
+genai.configure(api_key="AIzaSyAjwSb3D1loKnHrlb6NxzjcnC1nSHjOCCk")  
 genai_model = genai.GenerativeModel("gemini-1.5-flash")
 
 def getReason(verdict, weights):
